ea0fac395aa4da806c3ce2a8a96e241120315eff
mici <60635066+midorimici@users.noreply.github.com>
2024-08-03 10:33:15 +0900

    test

--- .github/script.sh
+++ .github/script.sh
@@ -4,6 +4,17 @@ A_CHANGED_FILES="$1"
 B_C_CHANGED_FILES="$2"
 B_D_CHANGED_FILES="$3"
 
-echo "$A_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
-echo "$B_C_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
-echo "$B_D_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
+if [ -n "$A_CHANGED_FILES" ]; then
+  echo "$A_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
+  echo
+fi
+
+if [ -n "$B_C_CHANGED_FILES" ]; then
+  echo "$B_C_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
+  echo
+fi
+
+if [ -n "$B_D_CHANGED_FILES" ]; then
+  echo "$B_D_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
+  echo
+fi
