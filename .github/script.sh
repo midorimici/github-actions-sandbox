7ffbb544f34bfcd77d9ef246d20d2a8c81871512
mici <60635066+midorimici@users.noreply.github.com>
2024-08-03 10:43:59 +0900

    test

--- .github/script.sh
+++ .github/script.sh
@@ -3,6 +3,13 @@
 A_CHANGED_FILES="$1"
 B_C_CHANGED_FILES="$2"
 B_D_CHANGED_FILES="$3"
+A_CHANGED="$4"
+
+if [ "$A_CHANGED" == "true" ]; then
+  echo "A changed"
+else
+  echo "A not changed"
+fi
 
 if [ -n "$A_CHANGED_FILES" ]; then
   echo "$A_CHANGED_FILES" | xargs -n 1 dirname | uniq | xargs -i echo './{}' | tr '\n' ' '
